d9dbc90714f00db00ad5a4c0452070c042cd7f40
Stephen Montgomery <stevemonty@yahoo.com>
2020-08-20 11:42:35 +0300

    Initial

---
+++ FuncC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FuncCConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var y = F.ToString2(123);
+
+            Console.WriteLine(y);
+        }
+
+    }
+
+    public static class F
+    {
+        public static readonly Func<int, string> ToString2 = i => i.ToString();
+
+        public static readonly Func<int, Func<int, int>> Add = a => b => a + b;
+    }
+
+}
